a96c01011375d1e10fd7fab5083d3919f903b618
Piotr Trzpil <piotr.trzpil@gmail.com>
2017-02-04 21:41:24 +0000

    imitate helm tool

--- tpl.go
+++ tpl.go
@@ -4,45 +4,86 @@ import (
 	"bytes"
 	"fmt"
 	"io"
-	"log"
+	log "github.com/Sirupsen/logrus"
 	"os"
 	"text/template"
-
-	"gopkg.in/yaml.v2"
+	"github.com/urfave/cli"
+	"strings"
 )
 
-// Reads a YAML document from the values_in stream, uses it as values
-// for the tpl_files templates and writes the executed templates to
-// the out stream.
-func ExecuteTemplates(values_in io.Reader, out io.Writer, tpl_files ...string) error {
-	tpl, err := template.ParseFiles(tpl_files...)
-	if err != nil {
-		return fmt.Errorf("Error parsing template(s): %v", err)
-	}
-
-	buf := bytes.NewBuffer(nil)
-	_, err = io.Copy(buf, values_in)
-	if err != nil {
-		return fmt.Errorf("Failed to read standard input: %v", err)
-	}
-
-	var values map[string]interface{}
-	err = yaml.Unmarshal(buf.Bytes(), &values)
-	if err != nil {
-		return fmt.Errorf("Failed to parse standard input: %v", err)
-	}
-
-	err = tpl.Execute(out, values)
-	if err != nil {
-		return fmt.Errorf("Failed to parse standard input: %v", err)
-	}
-	return nil
+func handleDefault(defaultValue string, providedValue string) string {
+	return providedValue
 }
 
 func main() {
-	err := ExecuteTemplates(os.Stdin, os.Stdout, os.Args[1:]...)
-	if err != nil {
-		log.Println(err)
-		os.Exit(1)
+	log.SetLevel(log.WarnLevel)
+	app := cli.NewApp()
+
+	app.Flags = []cli.Flag {
+		cli.StringSliceFlag {
+			Name: "set",
+			Usage: "Set a template value (--set name=value)",
+		},
+		cli.BoolFlag {
+			Name: "debug",
+			Usage: "Debug output",
+		},
 	}
+
+	app.Action = func(c *cli.Context) error {
+		filePath := ""
+		debug := c.Bool("debug")
+		if debug {
+			log.SetLevel(log.DebugLevel)
+		}
+
+		if c.NArg() > 0 {
+			filePath = c.Args().Get(0)
+		} else {
+			log.Fatal("Missing arg: filePath")
+		}
+
+		funcs := template.FuncMap {
+			"default": handleDefault,
+		}
+		templ, err := template.New("cs.yaml").Funcs(funcs).ParseFiles(filePath)
+
+		if err != nil {
+			return fmt.Errorf("Error parsing template(s): %v", err)
+		}
+		valuesMap := map[string]string {}
+
+		setVariables := c.StringSlice("set")
+		log.Debug("Got params: ", len(setVariables))
+
+		for _, setVariable := range setVariables {
+			nameVal := strings.Split(setVariable, "=")
+			if (len(nameVal) != 2) {
+				log.Fatal("Invalid --set flag: ", nameVal)
+			}
+			valuesMap[nameVal[0]] = nameVal[1]
+			log.Debug("Substituting template: ", nameVal[0], " with value: ", nameVal[1])
+		}
+
+		finalValues := map[string]interface{} {
+			"Values": valuesMap,
+		}
+		if true {
+			var targetWriter io.Writer = os.Stdout
+			if debug {
+				targetWriter = bytes.NewBufferString("")
+			}
+
+			err = templ.Execute(targetWriter, finalValues)
+			if err != nil {
+				log.Error("Failed to execute template: ", err)
+			} else {
+				log.Debug("Executed correctly.")
+			}
+		}
+
+		return nil
+	}
+
+	app.Run(os.Args)
 }
